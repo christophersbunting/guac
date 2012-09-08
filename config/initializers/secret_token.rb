503e665fbcd628a716c6ebe99b11548245876c77
Chris Bunting <chris@homerun.com>
2012-09-07 21:59:05 -0700

    avocado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guac::Application.config.secret_token = '60e61a80f883c5125885e8fcdff71f60af7c0474ee8fa219d5f0bf48b9b3fdc118ce6ad64cfb6a18378adb1d1a541388934a380a328c837f3a0da11c17c144b2'
